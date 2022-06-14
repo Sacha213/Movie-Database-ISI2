af1ac651b7c61b0398483c99c5a1d2ba910623a7
LABROSSE LILIAN p1911249 <lilian.labrosse@etu.univ-lyon1.fr>
2022-06-14 15:52:01 +0200

    modif readme

---
+++ createUser.sql
@@ -0,0 +1,4 @@
+create database netflix;
+create user usernetflix@localhost identified by 'netflixAdmin';
+grant all privileges on netflix *.** to usernetflix@localhost with grant option;
+flush privileges;
